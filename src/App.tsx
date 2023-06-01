b8b6dc181692c6b9fbd243216ae1f9cc213b0612
Serena <serenaolivieri@riseup.net>
2023-06-01 21:10:02 +0200

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './styles/styles.css'
 
 const App = () => {
+  const name = 'sere'
   return (
     <div>
-      <h1>hello</h1>
+      <h1>hello {name}</h1>
+      <p>yes</p>
       <p>{process.env.NODE_ENV}</p>
       <p>{process.env.name}</p>
     </div>
