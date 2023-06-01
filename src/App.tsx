787ca91b349607cbf2182508da22599fc3064533
Serena <serenaolivieri@riseup.net>
2023-06-01 21:01:42 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import "./styles/styles.css"
+import './styles/styles.css'
 
 const App = () => {
-
   return (
     <div>
-        <h1>hello</h1>
-        <p>{process.env.NODE_ENV}</p>
-        <p>{process.env.name}</p>
+      <h1>hello</h1>
+      <p>{process.env.NODE_ENV}</p>
+      <p>{process.env.name}</p>
     </div>
   )
 }
